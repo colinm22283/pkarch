be1a1fced3cc64ba7d04541d1724e426f43b6938
Colin McGann <colinm22283@gmail.com>
2026-05-11 01:48:14 -0500

    Flake completed

--- flake.nix
+++ flake.nix
@@ -2,37 +2,27 @@
   description = "PKArch Developement Flake";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.11";
+    nixpkgs.url = "nixpkgs/nixos-25.11";
+    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, ... }: let 
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      nativeBuildInputs = with pkgs; [
-        gcc
-        gdb
-        gnumake
-        pkg-config
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        
+        # Access the RISC-V 32-bit cross-compilation toolchain
+        riscvPkgs = pkgs.pkgsCross.riscv32-embedded;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.verilator
+            pkgs.gtkwave
 
-        vulkan-headers
-        vulkan-loader
-        vulkan-validation-layers
-
-        shaderc
-
-        freeglut
-        glfw
-        glm
-      ];
-    };
-
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      nativeBuildInputs = with pkgs; [
-        binutils
-      ];
-    };
-  };
+            riscvPkgs.buildPackages.binutils
+          ];
+        };
+      });
 }
 
